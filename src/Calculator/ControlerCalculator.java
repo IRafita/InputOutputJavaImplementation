b2f639a852d9239bdfe6f7c9b141497cab904ddc
TresLos <asistare7@gmail.com>
2015-03-23 06:46:57 +0100

    Preparant el Shuting Yard Algorithm

---
+++ src/Calculator/ControlerCalculator.java
@@ -0,0 +1,10 @@
+package src.Calculator;
+
+/* El Controlador superior */
+import src.ControlerCentral;
+
+/* Els sub Controladors */
+
+public class ControlerCalculator
+{
+}
---
+++ src/Understand/ShuntingYardAlgorithm/ShuntingYardAlgorithm.java
@@ -0,0 +1,14 @@
+package src.Understand.ShuntingYardAlgorithm;
+
+/* Classes propies de Java que necessito */
+import java.util.ArrayList;
+
+public class ShuntingYardAlgorithm
+{
+/* Classes necessaries per aquesta classe */
+	ControlerShuntingYardAlgorithm e;
+	public ShuntingYardAlgorithm (ControlerShuntingYardAlgorithm e)
+	{
+		up = e;
+	}
+}
