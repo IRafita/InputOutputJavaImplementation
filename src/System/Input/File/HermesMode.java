870ab7b825be49d1f00cec83d681239aad2c0830
TresLos <asistare7@gmail.com>
2015-04-01 07:04:56 +0200

    fins i tot la documentacio compila

--- src/System/Input/File/HermesMode.java
+++ src/System/Input/File/HermesMode.java
@@ -1,3 +1,4 @@
+/*
 Idea/proposta
 
 Deu greek, hermes
@@ -5,3 +6,4 @@ Deu greek, hermes
 Aquest si se vol communicar necessitara d'en modes. Per aixo ho he posat.
 
 Tambe, pel missatge d'error, he pensat en crear un hermes OUTPUT
+*/
